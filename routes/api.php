b09399285fc9fd9493cd6fb122f8fad98dc5c1fd
PedroCalazansTecnol <pedro.calazans@sistemastecnol.com.br>
2025-08-12 09:46:04 -0300

    Salvando alterações no api.php antes do pull

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,13 @@ use App\Http\Controllers\TimeController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::delete('time/{id}',[TimeController::class,'delete']);
+Route::delete('time/{id}/delete',[TimeController::class,'delete']);
 
 
 
 Route::post('/teste', function (Request $request) {
     return response()->json([
-        'mensagem' => 'API funcionando!',
-        'dados' => $request->all()
+        'mensagem' => 'API ativa!',
+      
     ]);
 });
