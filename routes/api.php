b10412124e7f7526eef15262c40b33677dc05116
PedroCalazansTecnol <pedro.calazans@sistemastecnol.com.br>
2025-08-12 09:21:28 -0300

    Corrigindo funcionalidades da API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use App\Http\Controllers\TimeController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Http\Request;
 
 Route::delete('time/{id}',[TimeController::class,'delete']);
+
+
+
+Route::post('/teste', function (Request $request) {
+    return response()->json([
+        'mensagem' => 'API funcionando!',
+        'dados' => $request->all()
+    ]);
+});
