aa658e8aa0b39391c041c51ec7b0b884c80feb76
Felipe Genistretti Rodrigues <genistrettifelipe@gmail.com>
2025-09-05 11:41:01 -0300

    Implementando redis como sistema de cache

--- app/Repositories/Eloquent/EloquentTimeRepository.php
+++ app/Repositories/Eloquent/EloquentTimeRepository.php
@@ -5,42 +5,65 @@ namespace App\Repositories\Eloquent;
 use App\Models\Time;
 use App\Repositories\Contracts\TimeRepositoryInterface;
 use Illuminate\Support\Facades\Cache;
-use InvalidArgumentException;
+
 
 class EloquentTimeRepository implements TimeRepositoryInterface {
+
     protected $cacheTTL = 3600;
+
+
     public function allWithJogadores(){
-        return Time::with('jogadores')->get();
+        return Cache::remember('times:with-jogadores', $this->cacheTTL, function(){
+            return Time::with('jogadores')->get();
+        });
     }
 
     public function createTime (array $data){
         $time = Time::create($data);
+        Cache::forget('times:all');
+        Cache::forget('times:with-jogadores');
         return $time;
     }
 
   
     public function deleteTime(Time $time){
-      
-        return $time->delete();
+        $result = $time->delete();
+
+        Cache::forget("time:{$time->id}");
+        Cache::forget("times:all");
+        Cache::forget("times:with-jogadores");
+
+        return $result;
+
     }
 
     public function updateTime(Time $time, array $data){
         $time->update($data);
+
+        Cache::put("time:{$time->id}", $time, $this->cacheTTL);
+        Cache::forget('times:all');
+        Cache::forget('times:with-jogadores');
+
         return $time;
     }
 
-    public function findById(int $id)
-    {
-        return Time::findOrFail($id);
+     public function findById(int $id){
+        return Cache::remember("time:{$id}", $this->cacheTTL, function() use ($id) {
+            return Time::findOrFail($id);
+        });
     }
 
     public function allTimes()
-    {
-        return Time::all(); 
+    {   
+        return Cache::remember("times:all", $this->cacheTTL, function(){
+            return Time::all(); 
+        });
     }
 
     public function timeComJogadoresById(int $id)
     {
-        return Time::with('jogadores')->findOrFail($id);
+        return Cache::remember("time:{$id}", $this->cacheTTL, function() use($id){
+            return Time::with('jogadores')->findOrFail($id);
+        });
     }
 }
